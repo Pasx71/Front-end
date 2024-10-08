1ecb0a680fa144b1c2a57561410dfa66d7f2a122
Pasx71 <80799158+Pasx71@users.noreply.github.com>
2024-10-08 14:37:47 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My React Frontend</h1>
+        <p>Edit src/App.js and save to reload.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
